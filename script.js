be0c4ba45eea40dba878e7b7f00824f2c6ec9eb8
Sonia Uduma <sohnyauduma@gmail.com>
2021-08-16 17:31:59 +0100

    updated resume

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ tl.from(".btn-1", { y: 400, opacity: 0, duration: 1.5 })
   .from(".btn-4", { y: 400, opacity: 0, duration: 1.5 }, "-=1.4")
   .from(".btn-5", { y: 400, opacity: 0, duration: 1.5 }, "-=1.3")
   .from(".btn-6", { y: 400, opacity: 0, duration: 1.5 }, "-=1.3")
-  .from(".btn-7", { y: 400, opacity: 0, duration: 1.5 }, "-=1.2");
+  .from(".btn-7", { y: 400, opacity: 0, duration: 1.5 }, "-=1.2")
+  .from(".btn-8", { y: 400, opacity: 0, duration: 1.5 }, "-=1.1")
+  .from(".btn-9", { y: 400, opacity: 0, duration: 1.5 }, "-=1.0");
 
 let tl_2 = gsap.timeline({
   scrollTrigger: {
