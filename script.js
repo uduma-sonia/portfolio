d1be365e234cef6531ef49d3da0f54922a0e3b68
Sonia Uduma <sohnyauduma@gmail.com>
2021-11-10 13:33:57 +0100

    changed UI, minmal design

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ tl.from(".btn-1", { y: 400, opacity: 0, duration: 1.5 })
   .from(".btn-6", { y: 400, opacity: 0, duration: 1.5 }, "-=1.3")
   .from(".btn-7", { y: 400, opacity: 0, duration: 1.5 }, "-=1.2")
   .from(".btn-8", { y: 400, opacity: 0, duration: 1.5 }, "-=1.1")
-  .from(".btn-9", { y: 400, opacity: 0, duration: 1.5 }, "-=1.0");
+  .from(".btn-9", { y: 400, opacity: 0, duration: 1.5 }, "-=1.0")
+  .from(".btn-10", { y: 400, opacity: 0, duration: 1.5 }, "-=0.9")
+  .from(".btn-11", { y: 400, opacity: 0, duration: 1.5 }, "-=0.8");
 
 let tl_2 = gsap.timeline({
   scrollTrigger: {
